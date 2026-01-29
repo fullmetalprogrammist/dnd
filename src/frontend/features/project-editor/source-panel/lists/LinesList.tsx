93b542378a43731dd1c502f64507924e8181ac1a
fmp <come.as@you.are>
2026-01-29 10:34:20 +0300

    Раскидал по папкам фронт более-менее

--- src/frontend/features/project-editor/source-panel/lists/LinesList.tsx
+++ src/frontend/features/project-editor/source-panel/lists/LinesList.tsx
@@ -1,5 +1,5 @@
 import { ProjectEditLineDto } from "@/src/backend/application/query/project/getProjectForEdit/ProjectEditDto"
 
 export function LinesList({ lines }: { lines: ProjectEditLineDto[]}) {
-  return lines.map(line => <div>{line.text}</div>)
+  return lines.map(line => <div key={line.id}>{line.text}</div>)
 }
